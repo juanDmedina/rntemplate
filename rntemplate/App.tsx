bb84e9877ee04b5b6c982031625c8132c0c27620
Juan Diego Medina Rojas <juan.medina@sofka.com.co>
2023-03-05 17:15:29 -0500

    Add project

---
+++ rntemplate/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text style={{fontSize:30, color: 'blue'}}>React Native Template TypeScript</Text>
+    </View>
+  );
+};
+
+export default App;
